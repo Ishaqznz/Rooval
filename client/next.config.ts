de2894292e8a0ee9bc058bd01f7284be64298a37
Ishaquel Aman <ishaquelaman@gmail.com>
2026-06-08 15:36:40 +0530

    change next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,32 +1,7 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   output: "export",
-
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Remove output: "export" so Vercel can handle your dynamic [id] routes */
-  
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  /* Your other valid config options go here if you have any */
 };
 
 export default nextConfig;
